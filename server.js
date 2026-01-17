a9b9502e115bb97757828d50aa908fc5cd785f62
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-01-17 16:26:52 +0530

    video 4 model and hashing password mern app

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 // configure env
 dotenv.config();
 
 // database config
 connectDB();
+
 // rest object
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/auth", authRoutes);
 
 // rest api
 app.get("/", (req, res) => {
