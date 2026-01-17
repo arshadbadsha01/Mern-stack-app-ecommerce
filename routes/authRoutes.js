eda85421bc3a39017650c3f830429b11d0852671
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-01-18 00:45:57 +0530

    video 7 protected middleware mern stack app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   registerController,
   loginController,
+  testController,
 } from "../controllers/authControllers.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddlewares.js";
 
 // router object
 const router = express.Router();
@@ -13,4 +15,8 @@ router.post("/register", registerController);
 
 // login || post
 router.post("/login", loginController);
+
+// test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
 export default router;
