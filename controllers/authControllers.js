ac9fd69ca970afd07a5499a0b3ca7c71a00d2085
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-01-21 02:06:32 +0530

    video 11 Login Register mern stack app

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -7,19 +7,19 @@ export const registerController = async (req, res) => {
     const { name, email, password, phone, address } = req.body;
     // validation
     if (!name) {
-      return res.send({ error: "Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
-      return res.send({ error: "email is Required" });
+      return res.send({ message: "email is Required" });
     }
     if (!password) {
-      return res.send({ error: "password is Required" });
+      return res.send({ message: "password is Required" });
     }
     if (!phone) {
-      return res.send({ error: "phone no is Required" });
+      return res.send({ message: "phone no is Required" });
     }
     if (!address) {
-      return res.send({ error: "address is Required" });
+      return res.send({ message: "address is Required" });
     }
 
     // check user
@@ -28,7 +28,7 @@ export const registerController = async (req, res) => {
     // existing user
     if (existingUser) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: "Already Register Please Login",
       });
     }
