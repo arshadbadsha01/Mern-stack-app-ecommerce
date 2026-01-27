28d0e28fb08f34cfdbc568d1cdbc55de4e3819d7
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-01-27 18:13:38 +0530

    video 21 category price pagination filters mern stack app

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 8080;
 // run listen
 app.listen(PORT, () => {
   console.log(
-    `server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+    `server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgYellow
       .white,
   );
 });
