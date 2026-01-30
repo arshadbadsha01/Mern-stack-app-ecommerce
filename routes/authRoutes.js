9d60ca810b2fef4c8e0db57314db5cee2ddf3701
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-01-30 22:07:07 +0530

    video 25 Manage user profile Mern stack app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ import {
   loginController,
   testController,
   forgotPasswordController,
+  updateProfileController,
 } from "../controllers/authControllers.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddlewares.js";
 
@@ -27,9 +28,12 @@ router.get("/test", requireSignIn, isAdmin, testController);
 router.get("/user-auth", requireSignIn, (req, res) => {
   res.status(200).send({ ok: true });
 });
+
 //protected Admin route auth
 router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
 
+// update profile
+router.put("/profile", requireSignIn, updateProfileController);
 export default router;
