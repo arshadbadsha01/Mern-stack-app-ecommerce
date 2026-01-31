2a5d77db5f35a6fac1903bb007e521114dd33a34
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-01-31 16:02:48 +0530

    video 26 Payment ateway Integration Mern stack app

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  brainTreePaymentController,
+  brainTreeTokenController,
   createProductController,
   deleteProductController,
   getProductController,
@@ -64,5 +66,12 @@ router.get("/related-product/:pid/:cid", relatedProductController);
 
 //category wise product
 router.get("/product-category/:slug", productCategoryController);
+
+//payments routes
+//token
+router.get("/braintree/token", brainTreeTokenController);
+
+//payments
+router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
 
 export default router;
